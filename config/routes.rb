b969a16d206b6357da8bea24e9cf02725b12edbd
Joyce Martinez <joycetezanos@gmail.com>
2020-08-11 16:02:31 -0500

    Rails Task Manager done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'index', to: 'tasks#index', as: :tasks
+    post "index", to: "tasks#create"
+    get 'tasks/new', to: 'tasks#new' # The `new` route needs to be *before* `show` route.
+    get 'index/:id/edit', to: 'tasks#edit', as: :edit_task
+    patch "tasks/:id", to: "tasks#update"
+    get 'tasks/:id', to: 'tasks#show', as: :task
+    delete 'tasks/:id', to: "tasks#destroy"
 end
